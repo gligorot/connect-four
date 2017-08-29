81783b4594f727137f168287b235465c353cd564
Gligor <dracevo.kromid@gmail.com>
2017-08-29 12:42:20 +0200

    Add win_check, except for play method/polish basically finished

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -32,7 +32,7 @@ class Board
   end
 
   def make_move(column, *move_color)
-    #move_color ||= @one.color
+    move_color ||= @one.color
     depth = 5
     until depth < 0
       potential = @board[depth][column]
@@ -46,7 +46,103 @@ class Board
     end
   end
 
-  def check_draw #followed by return if check draw == true
+  def draw_check #followed by return if check draw == true
+    if @board[0].all? { |cell| !(cell.color.empty?)}
+      return true
+    else
+      return false
+    end
   end
 
+  def win_check
+    horizontal_win_check("⚫")
+    horizontal_win_check("⚪")
+    vertical_win_check("⚫")
+    vertical_win_check("⚪")
+    diagonal_win_check("⚫")
+    diagonal_win_check("⚪")
+
+    #should execute if none stops/returns
+    return false
+  end
+
+  def horizontal_win_check(color)
+    @board.each do |row|
+      if row[0..3].all? {|x| x.color[0] == color}
+        return true
+        return
+      end
+      if row[1..4].all? {|x| x.color[0] == color}
+        return true
+        return
+      end
+      if row[2..5].all? {|x| x.color[0] == color}
+        return true
+        return
+      end
+      if row[3..6].all? {|x| x.color[0] == color}
+        return true
+        return
+      end
+    end
+
+  end
+
+  def vertical_win_check(color)
+    columns = []
+
+    7.times do |index|
+      columns << @board.collect {|row| row[index]}
+    end
+
+    columns.each do |col|
+      if col[0..3].all? {|x| x.color[0] == color}
+        return true
+        return
+      end
+      if col[1..4].all? {|x| x.color[0] == color}
+        return true
+        return
+      end
+      if col[2..5].all? {|x| x.color[0] == color}
+        return true
+        return
+      end
+    end
+  end
+
+  def diagonal_win_check(color)
+    diagonals = diagonal_generator
+
+    diagonals.each do |diagonal|
+      if diagonal.all? {|x| x.color[0] == color}
+        return true
+        return
+      end
+    end
+  end
+
+  def diagonal_generator
+    diagonals = []
+    depth = 0
+
+    until depth == 3
+      left = 0
+      right = 6
+      4.times do
+        diagonals << [@board[depth+0][left+0], @board[depth+1][left+1], @board[depth+2][left+2], @board[depth+3][left+3]]
+        diagonals << [@board[depth+0][right-0], @board[depth+1][right-1], @board[depth+2][right-2], @board[depth+3][right-3]]
+
+        left+=1
+        right-=1
+      end
+      depth += 1
+    end
+
+    diagonals
+  end
+
+  def switch_players
+    @one, @two = @two, @one
+  end
 end
--- spec/cfour_spec.rb
+++ spec/cfour_spec.rb
@@ -3,7 +3,7 @@ require "connect_four"
 describe Board do
   let(:game) {Board.new}
 
-  describe "#make move" do
+  describe "#make_move" do
     context "valid move" do
       it "correctly makes and displays a move" do
         game.make_move(0, "⚫")
@@ -43,5 +43,99 @@ describe Board do
     end
   end
 
+  describe "#draw_check" do
+    context "while moves are less than 42; an empty stack exists" do
+      it "returns false" do
+        #random moves
+        game.make_move(0, "⚫")
+        game.make_move(3, "⚪")
 
+        expect(game.draw_check).to be false
+      end
+    end
+
+    context "when every stack is full" do
+      it "returns true" do
+        top_row = game.instance_variable_get(:@board)[0]
+        top_row.each {|cell| cell.color = "X"} #simulating full stacks
+
+        expect(game.draw_check).to be true
+      end
+    end
+  end
+
+  describe "#win_check" do
+    context "if win condition is not fulfilled" do
+      it "returns false" do
+        #random moves
+        game.make_move(0, "⚫")
+        game.make_move(3, "⚪")
+
+        expect(game.win_check).to be false
+        #do a NOT TO BE here
+      end
+    end
+
+    context "if win condition is fulfilled with" do
+      context "horizontal win" do
+        it "returns true" do
+          game.make_move(0, "⚫")
+          game.make_move(1, "⚫")
+          game.make_move(2, "⚫")
+          game.make_move(3, "⚫")
+
+          expect(game.horizontal_win_check("⚫")).to be true
+        end
+
+        it "returns true" do
+          game.make_move(3, "⚪")
+          game.make_move(4, "⚪")
+          game.make_move(5, "⚪")
+          game.make_move(6, "⚪")
+
+          expect(game.horizontal_win_check("⚪")).to be true
+          #expect(game.horizontal_win_check("⚫")).not_toto be true
+        end
+      end
+
+      context "vertical win" do
+        it "returns true" do
+          game.make_move(0, "⚫")
+          game.make_move(0, "⚫")
+          game.make_move(0, "⚫")
+          game.make_move(0, "⚫")
+
+          expect(game.vertical_win_check("⚫")).to be true
+          #expect(game.vertical_win_check("⚪")).not_to be true
+        end
+
+        it "returns true" do
+          game.make_move(1, "⚪")
+          game.make_move(1, "⚪")
+          game.make_move(1, "⚪")
+          game.make_move(1, "⚪")
+
+          expect(game.vertical_win_check("⚪")).to be true
+        end
+      end
+
+      context "diagonal win" do
+        it "returns true" do
+          game.make_move(1, "⚪")
+          game.make_move(2, "⚪")
+          game.make_move(2, "⚪")
+          game.make_move(3, "⚪")
+          game.make_move(3, "⚪")
+          game.make_move(3, "⚪")
+
+          game.make_move(0, "⚫")
+          game.make_move(1, "⚫")
+          game.make_move(2, "⚫")
+          game.make_move(3, "⚫")
+
+          expect(game.diagonal_win_check("⚫")).to be true
+        end
+      end
+    end
+  end
 end
